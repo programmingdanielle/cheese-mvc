3b3d012f7e00562c099b54fc3282e460f9eb1fd9
Danielle Bernhoester <danibernhoester@gmail.com>
2019-08-12 16:03:00 -0500

    worked on passing data and made a submit form

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -3,16 +3,40 @@ package org.launchcode.cheesemvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
 
 @Controller
 @RequestMapping("cheese")
 public class CheeseController {
 
+    static ArrayList<String> cheeses = new ArrayList();
+
     @RequestMapping(value = "")
     public String index(Model model) {
+
+        model.addAttribute("cheeses", cheeses);
         model.addAttribute("title", "My Cheeses");
-        /* We named the attribute in our controller "title"
-         */
         return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        // Essentially will just render the form
+        // Remember to add Model model to public String method to use model.addAttribute
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName) {
+        cheeses.add(cheeseName);
+
+        // Redirect to /cheese
+        return "redirect:";
     }
 }
